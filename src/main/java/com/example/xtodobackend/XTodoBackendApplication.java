91af7b8940792b8da6f8f4f85f1ab4615da9dae6
Hugo Zheng <zhenghugo515@gmail.com>
2020-08-04 20:48:11 -0400

    first commit

---
+++ src/main/java/com/example/xtodobackend/XTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.xtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XTodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xtodobackend/XTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
